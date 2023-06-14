33ab47ffeb44f518cf09e4d3634bd9707d7ad972
kanishk123agarwal <kanishkagarwal208@gmail.com>
2023-06-14 14:12:23 +0530

    Change

--- config/db.js
+++ config/db.js
@@ -5,17 +5,17 @@ const db = process.env.MONGO_URL;
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(
-        //     "mongodb+srv://"+
-        //     username +
-        //     ":" +
-        //     password +
-        //     "@cluster0.c3gy0no.mongodb.net/?retryWrites=true&w=majority"
-        //     , {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        // });
-        await mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true});
+        await mongoose.connect(
+            "mongodb+srv://"+
+            username +
+            ":" +
+            password +
+            "@cluster0.c3gy0no.mongodb.net/?retryWrites=true&w=majority"
+            , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        // await mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true});
 
         console.log("MongoDB is connected");
     } catch (err) {
